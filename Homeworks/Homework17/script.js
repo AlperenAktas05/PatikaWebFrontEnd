7877988dc898911c90564ed9ff9ff2fa094d3843
Alperen <77103498+AlperenAktas05@users.noreply.github.com>
2023-12-14 18:20:31 +0300

    Update script.js

--- Homeworks/Homework17/script.js
+++ Homeworks/Homework17/script.js
@@ -1,138 +1,171 @@
-// İnput verisini almak ve listeye eklemek
-let formDOM = document.querySelector("#input");
-formDOM.addEventListener("submit", formSubmit);
+//Görevi diziye ekleme
+let inputFormDOM = document.querySelector("#inputForm")
 
-let taskListDOM = document.querySelector("#taskList");
+inputFormDOM.addEventListener("submit", addTask)
 
-let taskArray = [];
-let counter = 0;
+let taskArray = []
+let counter = 0
 
-let alertDOM = document.querySelector("#alert");
+let alertDOM = document.querySelector("#alert")
 
-// Sayfa yüklendiğinde local storage'dan verileri al
 document.addEventListener("DOMContentLoaded", () => {
-    let storedTasks = localStorage.getItem("tasks");
-    if (storedTasks) {
-        taskArray = JSON.parse(storedTasks);
-        updateTaskList();
-    }
+  let storedTasks = localStorage.getItem("tasks")
+  if (storedTasks) {
+    taskArray = JSON.parse(storedTasks)
+    counter = taskArray.length
+    addDocument()
+    clickDelete()
+  }
 });
 
-function formSubmit(event) {
-    event.preventDefault();
+function addTask(event) {
+  event.preventDefault();
 
-    let inputTextDOM = document.querySelector("#inputText");
+  let inputDOM = document.querySelector("#input");
 
-    if (!inputTextDOM.value) {
-        alertDOM.innerHTML = `
-        <div class="position-fixed bottom-0 right-0 p-3" style="z-index: 5; right: 0; bottom: 0;">
-        <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">
-          <div class="toast-header">
-            <strong class="mr-auto">Uyarı!</strong>
-            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="toast-body">
-            Lütfen Görev Giriniz!
-          </div>
-        </div>
-      </div>
-        `;
-
-        let toast = new bootstrap.Toast(document.getElementById('liveToast'));
-        toast.show();
-
-        return;
-    }
-    else{
-        alertDOM.innerHTML = `
-        <div class="position-fixed bottom-0 right-0 p-3" style="z-index: 5; right: 0; bottom: 0;">
-        <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">
-          <div class="toast-header">
-            <strong class="mr-auto">Bildirim</strong>
-            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="toast-body">
-            Görev Eklendi.
-          </div>
-        </div>
-      </div>
-        `;
-
-        let toast = new bootstrap.Toast(document.getElementById('liveToast'));
-        toast.show();
-    }
-
-    taskArray[counter] = inputTextDOM.value;
-
-    let taskListIndexDOM = document.createElement("li");
-    taskListIndexDOM.classList.add("list-group-item");
-    taskListIndexDOM.innerHTML = `
-        <form id="inputList${counter}" class="d-flex align-items-center">
-            <span>${taskArray[counter]}</span>
-             <button type="button" class="input-group-text ml-auto btn-delete" data-index="${counter}">X</button>
-        </form>
-    `;
-    taskListDOM.append(taskListIndexDOM);
-    inputTextDOM.value = "";
-    counter++;
-    updateLocalStorage();
-    attachDeleteEvent(); // Yeni form ekledikten sonra silme olayını tekrar bağla
-}
-
-// Listeden veri silmek
-function attachDeleteEvent() {
-    let deleteButtons = document.querySelectorAll(".btn-delete");
-    deleteButtons.forEach(button => {
-        button.addEventListener("click", formDelete);
-    });
-}
-
-function formDelete(event) {
-    let indexToDelete = event.target.getAttribute("data-index");
-    taskArray.splice(indexToDelete, 1); // Diziden elemanı kaldır
-    updateLocalStorage();
-    updateTaskList(); // Liste güncelle
+  if(!inputDOM.value){
     alertDOM.innerHTML = `
     <div class="position-fixed bottom-0 right-0 p-3" style="z-index: 5; right: 0; bottom: 0;">
     <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">
       <div class="toast-header">
-        <strong class="mr-auto">Bildirim</strong>
+        <strong class="mr-auto">Uyarı!</strong>
         <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="toast-body">
-        Görev Silindi.
+        Lütfen Görev Giriniz!
       </div>
     </div>
-  </div>
-    `;
+    </div>
+    `
 
     let toast = new bootstrap.Toast(document.getElementById('liveToast'));
     toast.show();
+
+    return
+  }
+
+  taskArray[counter] = {
+    task: `${inputDOM.value}`,
+  };
+
+  counter++;
+  console.log(taskArray);
+  inputDOM.value = "";
+
+  alertDOM.innerHTML = `
+  <div class="position-fixed bottom-0 right-0 p-3" style="z-index: 5; right: 0; bottom: 0;">
+  <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">
+    <div class="toast-header">
+      <strong class="mr-auto">Bildirim</strong>
+      <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="toast-body">
+      Görev Eklendi.
+    </div>
+  </div>
+  </div>
+  `
+
+  let toast = new bootstrap.Toast(document.getElementById('liveToast'));
+  toast.show();
+
+  addDocument();
+  clickDelete()
+  saveToLocalStorage()
 }
 
-function updateTaskList() {
-    taskListDOM.innerHTML = ""; // Liste temizle
-    taskArray.forEach((task, index) => {
-        let taskListIndexDOM = document.createElement("li");
-        taskListIndexDOM.classList.add("list-group-item");
-        taskListIndexDOM.innerHTML = `
-            <form id="inputList${index}" class="d-flex align-items-center">
-                <span>${task}</span>
-                 <button type="button" class="input-group-text ml-auto btn-delete" data-index="${index}">X</button>
-            </form>
-        `;
-        taskListDOM.append(taskListIndexDOM);
+function addDocument() {
+  let taskListDOM = document.querySelector("#taskList");
+  taskListDOM.innerHTML = ""
+
+  taskArray.forEach((item, index, array) => {
+    let taskListLiDOM = document.createElement("li");
+    taskListLiDOM.classList.add("list-group-item");
+
+    let deleteButtonDOM = document.createElement("button");
+    deleteButtonDOM.classList.add("float-right", "btn", "border", "border-1", "delete-button");
+    deleteButtonDOM.innerHTML = "X";
+
+    taskListLiDOM.innerHTML = item.task
+    taskListLiDOM.append(deleteButtonDOM);
+
+    taskListDOM.append(taskListLiDOM);
+  });
+}
+
+// Görev silme
+function clickDelete() {
+  let deleteButtonsDOM = document.querySelectorAll(".delete-button");
+
+  deleteButtonsDOM.forEach((item, index, array) => {
+    item.removeEventListener("click", function() {
+      deleteTask(index);
     });
-    attachDeleteEvent(); // Silme olayını tekrar bağla
+  });
+
+  deleteButtonsDOM.forEach((item, index, array) => {
+    item.addEventListener("click", function() {
+      deleteTask(index);
+    });
+  });
 }
 
-function updateLocalStorage() {
-    // Verileri JSON formatına çevirip local storage'a kaydet
-    localStorage.setItem("tasks", JSON.stringify(taskArray));
+
+function deleteTask(index) {
+  taskArray.splice(index, 1)
+  console.log(taskArray)
+
+  updateDocument()
+
+  counter = 0
+
+  alertDOM.innerHTML = `
+  <div class="position-fixed bottom-0 right-0 p-3" style="z-index: 5; right: 0; bottom: 0;">
+  <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true" data-delay="2000">
+    <div class="toast-header">
+      <strong class="mr-auto">Bildirim</strong>
+      <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="toast-body">
+      Görev Silindi.
+    </div>
+  </div>
+  </div>
+  `
+
+  let toast = new bootstrap.Toast(document.getElementById('liveToast'));
+  toast.show();
+
+  saveToLocalStorage()
+}
+
+function updateDocument(){
+  taskListDOM = document.querySelector("#taskList");
+  taskListDOM.innerHTML = ""
+
+  taskArray.forEach((item, index, array) => {
+    taskListLiDOM = document.createElement("li");
+    taskListLiDOM.classList.add("list-group-item");
+
+    deleteButtonDOM = document.createElement("button");
+    deleteButtonDOM.classList.add("float-right", "btn", "border", "border-1", "delete-button");
+    deleteButtonDOM.innerHTML = "X";
+
+    taskListLiDOM.innerHTML = item.task;
+    taskListLiDOM.append(deleteButtonDOM);
+
+    taskListDOM.append(taskListLiDOM);
+  });
+
+  clickDelete()
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("tasks", JSON.stringify(taskArray));
 }
